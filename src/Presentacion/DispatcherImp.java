f3e9dc9ccaa728c1832bf0e2ff6303ee78710e2e
hecoding <hlaria@ucm.es>
2014-12-07 21:53:47 +0100

    nueva generación de código

---
+++ src/Presentacion/DispatcherImp.java
@@ -0,0 +1,24 @@
+package Presentacion;
+/**
+ * 
+ */
+
+/** 
+ * <!-- begin-UML-doc -->
+ * <!-- end-UML-doc -->
+ * @author Héctor
+ * @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+ */
+public class DispatcherImp extends Dispatcher {
+	/** 
+	 * <!-- begin-UML-doc -->
+	 * <!-- end-UML-doc -->
+	 * @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void dispatch() {
+		// begin-user-code
+		// TODO Apéndice de método generado automáticamente
+
+		// end-user-code
+	}
+}
